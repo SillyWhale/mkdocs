b5b5e1f6475d3690097fef17f8cefd621b19caa0
Vincent FRICOU <vincent+github@fricouv.eu>
2018-06-12 09:21:06 +0200

    Update init file to create default project if not exist yet

--- include/init.sh
+++ include/init.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 cd ${MKD_ROOT}
+CFG_EXIST=$(find ./ -type f -name "mkdocs.yml")
+if [ -z ${CFG_EXIST} ]
+then
+    mkdocs new project
+    mv project/* .
+    rmdir project
+fi
 /usr/bin/mkdocs serve
